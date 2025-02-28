09624444005c852b8a52d9bdf35d88fa7b705959
Diego <diegohenrique123012@gmail.com>
2025-02-28 20:32:49 -0300

    chore(backend/database): modificando nome dos documents que seram enviados ao mongoDB com base nos models criados dentro de database

--- backend/database/config.py
+++ backend/database/config.py
@@ -1,6 +1,6 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from models import Moto, Proprietario, Manutencao  # Importando os modelos
+from database.models import Maintenance, Motorcycle, Owner, Admin # Importando os modelos
 import os
 from dotenv import load_dotenv
 
@@ -11,4 +11,4 @@ MONGO_URI = os.getenv("MONGO_URI")
 async def init_db():
     client = AsyncIOMotorClient(MONGO_URI)
     database = client["sistema_manutencao"]
-    await init_beanie(database, document_models=[Moto, Proprietario, Manutencao])
+    await init_beanie(database, document_models=[Admin, Owner, Motorcycle, Maintenance])
